93d77980af3bd7b7f6c39c2667d6328298428d3b
chaimalang <chaimaarami717@gmail.com>
2025-07-07 01:13:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>مرحبًا بكم في موقع CHN</h1>
+      <p>الموقع يعمل بنجاح 🎉</p>
+    </div>
+  );
+}
